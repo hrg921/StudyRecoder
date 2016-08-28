2280197c97d276b28a1c0c1eeb6463c23e6761d1
hrg921 <hrg921@gmail.com>
2016-08-28 15:11:16 +0900

    Add initList functions

--- app/src/main/java/mp/hd3534/studyrecoder/ListActivity.java
+++ app/src/main/java/mp/hd3534/studyrecoder/ListActivity.java
@@ -2,20 +2,44 @@ package mp.hd3534.studyrecoder;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.AbsListView;
 import android.widget.ListView;
+
+import java.util.List;
+
+import mp.hd3534.studyrecoder.Data.StudyData;
+import mp.hd3534.studyrecoder.Database.DbController;
+import mp.hd3534.studyrecoder.List.ListViewAdapter;
 
 public class ListActivity extends Activity {
 
+    private DbController dbController;
+    private List<StudyData> studyDatas;
+
     private ListView listView;
+    private ListViewAdapter listViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        initData();
         initView();
+        initList();
     }
 
     private void initView() {
         listView = (ListView)findViewById(R.id.list_listview);
+    }
+
+    private void initData() {
+        dbController = new DbController(getApplicationContext());
+        studyDatas = dbController.selectAll();
+    }
+
+    private void initList() {
+        listViewAdapter = new ListViewAdapter(getApplicationContext(), R.layout.listview_item, (StudyData[])studyDatas.toArray());
+        listView.setChoiceMode(AbsListView.CHOICE_MODE_NONE);
+        listView.setAdapter(listViewAdapter);
     }
 }
